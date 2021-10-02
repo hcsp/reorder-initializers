86f17fc84b8e64fadbafd737480f375a16fa35f9
UsernameisnotavailableFeng <91192169+UsernameisnotavailableFeng@users.noreply.github.com>
2021-10-02 12:36:02 +0800

    按照顺序输出1~7 (#230)
    按照顺序输出1~7 (#230)

--- src/main/java/com/github/hcsp/objectbasic/Cat.java
+++ src/main/java/com/github/hcsp/objectbasic/Cat.java
@@ -1,20 +1,11 @@
 package com.github.hcsp.objectbasic;
 
 public class Cat {
-    public static int count = initStaticCount();
-
-    static {
-        System.out.println(1);
-    }
-
     int age = initAge();
     String name;
-
-    {
+    static {
+        System.out.println(1);
         System.out.println(2);
-    }
-
-    {
         System.out.println(3);
     }
 
@@ -22,20 +13,22 @@ public class Cat {
         System.out.println(4);
         return 0;
     }
-
+    public int count = initStaticCount();
     public static int initStaticCount() {
         System.out.println(5);
         return 0;
     }
 
     public Cat(String name) {
-        this(0, name);
+        age = 0;
+        this.name = name;
         System.out.println(6);
+        System.out.println(7);
     }
 
     public Cat(int age, String name) {
+        this(name);
         this.age = age;
-        this.name = name;
-        System.out.println(7);
+
     }
 }
