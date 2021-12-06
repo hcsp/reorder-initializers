c606bbda183e696216b5ad031d5cfe5258b43575
jcbound <784538725@qq.com>
2021-12-06 23:46:17 +0800

    调整顺序并展示

--- src/main/java/com/github/hcsp/objectbasic/Cat.java
+++ src/main/java/com/github/hcsp/objectbasic/Cat.java
@@ -1,20 +1,20 @@
 package com.github.hcsp.objectbasic;
 
 public class Cat {
-    public static int count = initStaticCount();
-
     static {
         System.out.println(1);
     }
 
+
     int age = initAge();
+    public int count = initStaticCount();
     String name;
 
-    {
+    static {
         System.out.println(2);
     }
 
-    {
+    static {
         System.out.println(3);
     }
 
@@ -23,19 +23,20 @@ public class Cat {
         return 0;
     }
 
-    public static int initStaticCount() {
+    public int initStaticCount() {
         System.out.println(5);
         return 0;
     }
 
     public Cat(String name) {
         this(0, name);
-        System.out.println(6);
+        System.out.println(7);
+
     }
 
     public Cat(int age, String name) {
         this.age = age;
         this.name = name;
-        System.out.println(7);
+        System.out.println(6);
     }
 }
