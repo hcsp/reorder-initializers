ee86dd802bc158b54d066e1b3146cea6fb276f90
bycgxss <bycgxss@gmail.com>
2022-04-02 22:21:52 +0800

    类中执行顺序

--- src/main/java/com/github/hcsp/objectbasic/Cat.java
+++ src/main/java/com/github/hcsp/objectbasic/Cat.java
@@ -1,14 +1,13 @@
 package com.github.hcsp.objectbasic;
 
 public class Cat {
-    public static int count = initStaticCount();
+    public static int count;
+    int age;
+    String name;
 
     static {
         System.out.println(1);
     }
-
-    int age = initAge();
-    String name;
 
     {
         System.out.println(2);
@@ -18,8 +17,21 @@ public class Cat {
         System.out.println(3);
     }
 
+    public Cat(String name) {
+        this(0, name);
+        System.out.println(7);
+    }
+
+    public Cat(int age, String name) {
+        this.age = this.initAge();
+        this.age = age;
+        this.name = name;
+        System.out.println(6);
+    }
+
     public int initAge() {
         System.out.println(4);
+        Cat.count = Cat.initStaticCount();
         return 0;
     }
 
@@ -28,14 +40,5 @@ public class Cat {
         return 0;
     }
 
-    public Cat(String name) {
-        this(0, name);
-        System.out.println(6);
-    }
 
-    public Cat(int age, String name) {
-        this.age = age;
-        this.name = name;
-        System.out.println(7);
-    }
 }
